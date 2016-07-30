6713e49ba10b6508b0cea41ffc1c8f377db78663
Rahul Kumar Singh <kumarahul120292@gmail.com>
2016-07-30 16:26:30 +0530

    Create Celebrity_jeopardy.cpp

---
+++ Celebrity_jeopardy.cpp
@@ -0,0 +1,31 @@
+//
+//  testfile.cpp
+//  
+//
+//  Created by RAHUL KUMAR SINGH on 30/07/16.
+//
+//
+
+
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+#include<iostream>
+#include<set>
+#include<utility>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+#define _CRT_SECURE_NO_DEPRECATE
+#define INF 1000000000
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+int main(){
+    std::string line;
+    while (std::getline(std::cin,line)) {
+        cout<<line<<endl;
+    }
+    return 0;
+}
